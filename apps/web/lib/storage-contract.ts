230f2f66c2295cc7c7d1c6b13927e0416d15f1b2
JWJeffery <josh@jwjeffery.org>
2026-07-05 17:00:49 -0700

    Add storage contract constants

---
+++ apps/web/lib/storage-contract.ts
@@ -0,0 +1,6 @@
+export const storageContract = {
+  databaseName: "scriptorium-file-store",
+  pdfStore: "pdf-blobs",
+  documentKey: "scriptorium.currentDocument",
+  annotationsKey: "scriptorium.annotations"
+} as const;
