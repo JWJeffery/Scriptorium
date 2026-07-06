c46dfbf70b126e0430e687e3f539d5766f69a044
JWJeffery <josh@jwjeffery.org>
2026-07-05 17:26:24 -0700

    Add Milestone 1 persistence types

---
+++ apps/web/lib/milestone-one-types.ts
@@ -0,0 +1,52 @@
+export type MilestoneOneSourceInput = {
+  author?: string;
+  title: string;
+  place?: string;
+  publisher?: string;
+  year?: string;
+};
+
+export type MilestoneOnePageMapInput = {
+  basePdfPageIndex: number;
+  baseBookPage: number;
+  currentPdfPageIndex: number;
+  bookPageLabel: string;
+};
+
+export type MilestoneOneDocumentInput = {
+  title: string;
+  filename: string;
+  mediaType: string;
+  size: number;
+  source: MilestoneOneSourceInput;
+  pageMap: MilestoneOnePageMapInput;
+};
+
+export type MilestoneOneAnchorInput = {
+  selectedText: string;
+  pageNumber: number;
+  beforeContext?: string;
+  afterContext?: string;
+  rects?: Array<{
+    left: number;
+    top: number;
+    width: number;
+    height: number;
+  }>;
+};
+
+export type MilestoneOneAnnotationInput = {
+  documentId: string;
+  versionId: string;
+  sourceId: string;
+  pageMapId?: string;
+  colorKey: string;
+  selectedText: string;
+  note?: string;
+  tags?: string[];
+  anchor?: MilestoneOneAnchorInput;
+  citationStyle: string;
+  citationText: string;
+  locatorType?: string;
+  locatorValue?: string;
+};
